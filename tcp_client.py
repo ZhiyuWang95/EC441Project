0d0d1e805bcd6c2d5e903f42d2930e085bdbddc0
Joseph-Zhiyu-Wang <31745564+ZhiyuWang95@users.noreply.github.com>
2018-12-07 16:05:58 -0500

    communication down

--- tcp_client.py
+++ tcp_client.py
@@ -12,15 +12,27 @@ sock = s.socket(s.AF_INET, s.SOCK_STREAM)
 #input('wait')
 sock.connect( (SERVER_ADDR, SERVER_PORT) )
 
-print('i am',sock.getsockname())
+print('Here is',sock.getsockname())
 
-while True:
-    val=int(input('give me a number to factorialize (0 to quit):'))
-    if val==0: break
+firstreply = sock.recv(SIZE_LIMIT)
+firsti = int.from_bytes(firstreply,'big')
+print('I am player{}'.format(firsti+1))
+print('Input move in "x, y" format.')
 
-    sock.send(val.to_bytes(4,'big'))
+if(firsti == 1):
+    print("Waiting player1 to make the first move")
     reply = sock.recv(SIZE_LIMIT)
     i = int.from_bytes(reply,'big')
-    print('{}! is'.format(val), i)
+    print('Player1 made move {}'.format(i))
+
+while True:
+    val=int(input('Make a move (0 to quit):'))
+    if val==0: break
+    sock.send(val.to_bytes(4,'big'))
+    print("...waiting player{} to make move...".format(2-firsti))
+
+    reply = sock.recv(SIZE_LIMIT)
+    i = int.from_bytes(reply,'big')
+    print('Player{} made move {}'.format(2-firsti, i))
 
 sock.close()
--- tcp_server.py
+++ tcp_server.py
@@ -3,7 +3,10 @@
 Calculate factorials for clients.
 """
 import math
+import multiprocessing
+import subprocess
 import socket as s
+from multiprocessing import Process, Pipe
 
 SIZE_LIMIT = 16
 SERVER_ADDR, SERVER_PORT = '127.0.0.1', 9000
@@ -14,18 +17,55 @@ server_soc = s.socket(s.AF_INET, s.SOCK_STREAM)
 server_soc.bind( (SERVER_ADDR, SERVER_PORT) )
 server_soc.listen(1)
 print('Server is listening.')
+clients = []
+connections = []
+parentpip = []
+
+def handle(connection, address, conn):
+    import logging
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger("process-%r" % (address,))
+    try:
+        logger.debug("Connected %r at %r", connection, address)
+        while True:
+            message = connection_soc.recv(SIZE_LIMIT)
+            val = int.from_bytes(message,'big')
+            if not val: break
+
+            fact_res = val #changed by ZY.
+            msg = fact_res.to_bytes(12,'big')
+            conn.send(fact_res)
+    except:
+        logger.exception("Problem handling request")
+
 
 while True:
-    connection_soc, addr  = server_soc.accept()
-    print('responding to ', addr)
-    while True:
-        message =  connection_soc.recv(SIZE_LIMIT)
-        val = int.from_bytes(message,'big')
-        if not val: break
+    if(len(clients) < 2):
+        connection_soc, addr = server_soc.accept()
+        info_pair = (connection_soc, addr)
+        index = len(clients)
+        indexmsg = index.to_bytes(12,'big')
+        connection_soc.send(indexmsg)
 
-        fact_res = math.factorial(val)
-        msg = fact_res.to_bytes(12,'big')
-        connection_soc.send(msg)
+        if(info_pair not in clients):
+            clients.append(info_pair)
+            connections.append(connection_soc)
 
-    connection_soc.close()
-    print('done')
+            parent_conn, child_conn = Pipe()
+
+            parentpip.append(parent_conn)
+            process = multiprocessing.Process(target=handle, args=(connection_soc, addr, child_conn))
+            process.daemon = True
+            process.start()
+            print('responding to ', addr)
+    else:
+        player1 = parentpip[0].recv()
+        if(player1):
+            move1 = player1.to_bytes(12,'big')
+            connections[1].send(move1)
+
+        player2 = parentpip[1].recv()
+        if(player2):
+            move2 = player2.to_bytes(12,'big')
+            connections[0].send(move2)
+
