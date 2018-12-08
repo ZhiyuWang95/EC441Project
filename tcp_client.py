f1f9d97db2ccfc2b50cacbad6f249a2e87284a3c
Zhiyu Wang <wangzy95@bu.edu>
2018-12-07 19:39:05 -0500

    quit function working

--- tcp_client.py
+++ tcp_client.py
@@ -3,6 +3,8 @@ send numbers to a server and display the calculation.
 """
 
 import socket as s
+import base64
+
 SIZE_LIMIT = 1024
 
 SERVER_ADDR, SERVER_PORT = '127.0.0.1', 9000
@@ -22,17 +24,25 @@ print('Input move in "x, y" format.')
 if(firsti == 1):
     print("Waiting player1 to make the first move")
     reply = sock.recv(SIZE_LIMIT)
-    i = int.from_bytes(reply,'big')
-    print('Player1 made move {}'.format(i))
+    reply_dec = reply.decode('utf-8')
+    if reply_dec == 'q':
+        print("Player1 quits. Game end.")
+        sock.close()
+    print('Player1 made move {}'.format(reply_dec))
 
 while True:
-    val=int(input('Make a move (0 to quit):'))
-    if val==0: break
-    sock.send(val.to_bytes(4,'big'))
-    print("...waiting player{} to make move...".format(2-firsti))
+    #val=int(input('Make a move (0 to quit):'))
+    strinput = input('Make a move (q to quit):')
+    str_enc = strinput.encode('utf-8')
+
+    sock.send(str_enc)
+    if strinput=='q': break
+    print("...waiting player{} to make move...\n".format(2-firsti))
 
     reply = sock.recv(SIZE_LIMIT)
-    i = int.from_bytes(reply,'big')
-    print('Player{} made move {}'.format(2-firsti, i))
+    reply_dec = reply.decode('utf-8')
+    if reply_dec == 'q': break
+
+    print('Player{} made move {}'.format(2-firsti, reply_dec))
 
 sock.close()
--- tcp_server.py
+++ tcp_server.py
@@ -32,20 +32,22 @@ def handle(connection, address, conn):
             val = int.from_bytes(message,'big')
             if not val: break
 
-            fact_res = val #changed by ZY.
-            msg = fact_res.to_bytes(12,'big')
+            fact_res = message #changed by ZY.
+            #msg = fact_res.to_bytes(12,'big')
             conn.send(fact_res)
     except:
         logger.exception("Problem handling request")
 
+def sendIndex(connection, clients):
+    index = len(clients)
+    index_msg = index.to_bytes(12,'big')
+    connection_soc.send(index_msg)
 
 while True:
     if(len(clients) < 2):
         connection_soc, addr = server_soc.accept()
         info_pair = (connection_soc, addr)
-        index = len(clients)
-        indexmsg = index.to_bytes(12,'big')
-        connection_soc.send(indexmsg)
+        sendIndex(connection_soc, clients)
 
         if(info_pair not in clients):
             clients.append(info_pair)
@@ -61,11 +63,21 @@ while True:
     else:
         player1 = parentpip[0].recv()
         if(player1):
-            move1 = player1.to_bytes(12,'big')
+            move1 = player1
             connections[1].send(move1)
+            if move1.decode('utf-8') == 'q':
+                print('player1 quit, game over.')
+                break
 
         player2 = parentpip[1].recv()
         if(player2):
-            move2 = player2.to_bytes(12,'big')
+            move2 = player2
             connections[0].send(move2)
+            if move2.decode('utf-8') == 'q':
+                print('player2 quit, game over.')
+                break
 
+
+for i in range(2):
+    connections[i].close()
+connections.clear()
